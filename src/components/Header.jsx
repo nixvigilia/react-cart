79f90d0688c0e7abd9a292e824d18983faca1483
nix <john.nicholai.vigilia@gmail.com>
2022-01-15 05:48:42 +0800

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import { FaShoppingCart } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+import {
+  Badge,
+  Button,
+  Container,
+  Dropdown,
+  FormControl,
+  Nav,
+  Navbar,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+// import { CartState } from "../context/Context";
+// import "./styles.css";
+
+const Header = () => {
+  // const {
+  //   state: { cart },
+  //   dispatch,
+  //   productDispatch,
+  // } = CartState();
+
+  return (
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+      <Container>
+        <Navbar.Brand>
+          <Link to="/">Shopping Cart</Link>
+        </Navbar.Brand>
+        <Navbar.Text className="search">
+          <FormControl
+            style={{ width: 500 }}
+            type="search"
+            placeholder="Search a product..."
+            className="m-auto"
+            aria-label="Search"
+          />
+        </Navbar.Text>
+        <Nav>
+          <Dropdown align="end">
+            <Dropdown.Toggle variant="success">
+              <FaShoppingCart color="white" fontSize="25px" />
+              <Badge>{10}</Badge>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu style={{ minWidth: 370 }}>
+              <span style={{ padding: 10 }}>Cart is Empty!</span>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
